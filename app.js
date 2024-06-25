15b7bc9b674129aee83651c5d7de17365f9d7297
Diego Armando Martinez Tapias <damt13a@gmail.com>
2024-06-25 08:27:56 -0500

    Lógica de programación Reto 3

---
+++ app.js
@@ -0,0 +1,53 @@
+
+
+// // Hemos llegado a otra lista de actividades (no obligatorias) para que practiques y refuerces aún más tu aprendizaje. ¿Vamos a hacerlo? Desafíos:
+alert("Ejercicio 1");
+
+let contador = 1;
+while (contador <= 10){
+alert(`El valor de contador es ${contador}`);
+contador = contador + 1;
+}
+
+alert("Fin del código 1");
+
+// // Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+alert("Ejercicio 2");
+
+let contador2 = 10;
+while (contador2 >= 1){
+alert(`El valor de contador es ${contador2}`);
+contador2 = contador2 - 1;
+}
+
+alert("Fin del código 2");
+
+// // Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+
+alert("Ejercicio 3");
+let numeroUsuario = parseInt(prompt('Ingrese un número positivo'));
+let contador3 = 1;
+console.log(typeof(numeroUsuario));
+alert(numeroUsuario);
+
+while (contador3 <=numeroUsuario ){
+    alert(`El valor de contador es ${contador3}`);
+    contador3 = contador3 + 1;
+}
+
+alert("Fin del código 3");
+
+// Crea un programa de cuenta regresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+
+alert("Ejercicio 4");
+let numeroIngreso = parseInt(prompt('Ingrese un número positivo'));
+let contador4 = 1;
+console.log(typeof(numeroIngreso));
+alert(numeroIngreso);
+
+while (contador4 <=numeroIngreso ){
+    alert(`El valor de contador es ${contador3}`);
+    contador3 = contador3 - 1;
+}
+alert("Fin del código 4");
+
